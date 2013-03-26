db9f17b8836fc218765aa895cd30921c1728f956
fred <sylwek1992@gmail.com>
2013-03-26 20:05:03 +0100

    client

--- client/filesender.py
+++ client/filesender.py
@@ -1,9 +1,15 @@
-import socket
+import socket, sys
 
-def sendfile(filepath, addr = 'localhost', port = 8765):
+def sendfile(filepath, addr = 'localhost', port = 8888):
     """Function for sending a file"""
-    file_stringed = open(filepath, 'U').read()
+    o = open(filepath, 'rb')
+    file_stringed = o.read()
+    o.close()
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((addr, port))
     s.sendall(file_stringed)
+    data = s.recv(1024)
+    s.close()
 
+if __name__ == '__main__':
+        sendfile(sys.argv[1])
