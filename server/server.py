895f6a69ad2e878fc245d92cba106a12ba820fdc
fred <sylwek1992@gmail.com>
2013-03-26 20:02:17 +0100

    coś już tam przesyła....

--- server/server.py
+++ server/server.py
@@ -1,15 +1,33 @@
-import socket
+import socket, time
 
-def reciever(port = 8765):
+def reciever(port = 8888):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(('', port)) #pusty string oznacza ze bierze localhost
     s.listen(1)
     conn, addr = s.accept()
     print(addr)
-    
+    data = ''
+    o = open('testfile.jpg', 'wb+')
     while 1:
-        data= conn.recv(1000000)
+        try:
+            data = conn.recv(1024)
+            conn.sendall(data)
+        except socket.errno, e:
+            pass
+        except IOError, e:
+            if e.errno == 32: 
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.bind(('', port)) #pusty string oznacza ze bierze localhost
+                s.listen(1)
+                conn, addr = s.accept()
+
         if not data: break
-        conn.sendall(data)
-    open('testfile.jpg', 'w').write(data)
+        o.write(data)
+        print data
+        
+    o.close()
     conn.close()
+    print("koniec")
+
+if __name__ == '__main__':
+    reciever()
