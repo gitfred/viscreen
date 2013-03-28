f235a0a312e889093b819fa849bb75d348f39288
Mateusz Flieger <mateuszflieger@gmail.com>
2013-03-28 13:41:15 +0100

    Klient

--- client/filesender.py
+++ client/filesender.py
@@ -1,16 +1,17 @@
+#!/usr/bin/env python3.2
 import socket, sys
 
 def sendfile(filepath, addr = 'localhost', port = 25002):
     """Function for sending a file"""
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((addr, port))
-    o = open(filepath, 'rb')
-    while 1:
-        file_stringed = o.read(1024)
-        s.sendall(file_stringed)
-        data = s.recv(1024)
-        if not data:break
-    s.close();o.close()
+    with open(filepath, 'rb') as img:
+        while True:
+            file_stringed = img.read(1024)
+            s.sendall(file_stringed)
+            if not file_stringed:
+                break
+        s.close()
 
 if __name__ == '__main__':
         sendfile(sys.argv[1])
