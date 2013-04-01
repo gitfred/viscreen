42774eddbb0184f6fceae5fc03e4fa3f803c3380
fred <sylwek1992@gmail.com>
2013-04-01 14:51:46 +0200

    trolololo2

--- client/filesender.py
+++ client/filesender.py
@@ -12,7 +12,8 @@ def getscreen(name = "sc", ext = "png"):
     if (screen != None):
         filename = '.'.join([name,ext])
         screen.save(filename, ext)
-    return filename
+        return filename
+    return None
 
 def sendfile(filepath, addr = 'localhost', port = 25002):
     """Function for sending a file"""
