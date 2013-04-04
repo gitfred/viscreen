1be6ab2f29854cb341ad19394d8681386aedca10
Mateusz Flieger <mateuszflieger@gmail.com>
2013-04-04 17:08:26 +0200

    Git ignore etc

--- server/server.py
+++ server/server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
-import socket, datetime 
+import socket, datetime, os
 
 def reciever(port = 25002):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -8,13 +8,15 @@ def reciever(port = 25002):
     conn, addr = s.accept()
     print(addr)
     now = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-    filepath = '%s, %s.png' % (addr[0], now)
+    filepath = '%s/%s, %s.png' % (addr[0], addr[0], now)
+    if not os.path.exists(addr[0]):
+        os.makedirs(addr[0])
     with open(filepath, 'wb+') as image:
         while True:
             data = conn.recv(1024)
             if not data: break
             image.write(data)
-        conn.close()
+    conn.close()
     print("koniec")
 
 if __name__ == '__main__':
